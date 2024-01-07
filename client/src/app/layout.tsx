13050a2a33f4b8c2098954d382ddc589ac4b22df
alperen <alperen.kapusuz@bulutmd.com>
2024-01-07 16:10:34 +0300

    Add Radix UI packages and implement theme provider and header component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import Header from "@/layout/Header";
+import Header from "@/layouts/Header";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -23,8 +24,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <Header/>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ModeToggle } from "./components/ModeToggle";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
@@ -6,13 +8,13 @@ const Header = () => {
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="text-center sm:text-left">
-            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+            <h1 className="text-2xl font-bold text-primary sm:text-3xl">
               VUTUTUNING
             </h1>
           </div>
-
           <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
-            {/* Kayıt olma butonları gelecek */}
+            <Button size="lg">Kayıt Ol</Button>
+            <ModeToggle />
           </div>
         </div>
       </div>
